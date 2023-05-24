3e8e646e39a714ea869607705bc2d97d3d6769e6
FancySun88 <oookkq@gmail.com>
2023-05-24 20:01:54 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
       </header>
+      <form>
+        <input
+          type="search"
+          placeholder="Please, type a city..."
+          autocomplete="off"
+        />
+
+        <input type="submit" value="Get a forecast" />
+      </form>
       <footer>
         Coded by I. Zhelezniak 2023
         <a href="https://github.com/FancySun88/weather-react">
